8e587447558977f3bf1396225f2f3483ba7c7fca
dnoc <drew.noct@gmail.com>
2023-12-11 14:18:49 -0500

    day10

---
+++ code/day10.rb
@@ -0,0 +1,30 @@
+class Graph
+  def initialize
+    lines = File.readlines("./input/day10.txt").map(&:chomp)
+
+
+  end
+
+  def distance_from_start
+
+  end
+
+  def longest_distance
+
+  end
+end
+
+class Node
+
+end
+# I really don't want to implement Dijkstra
+
+def part1
+  graph = Graph.new
+end
+part1
+
+def part2
+  graph = Graph.new
+end
+# part2
