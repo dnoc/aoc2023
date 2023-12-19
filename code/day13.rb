4d43e8dfa55c500c656f698649ac5ceb536e89a1
dnoc <drew.noct@gmail.com>
2023-12-19 11:23:00 -0500

    day13

--- code/day13.rb
+++ code/day13.rb
@@ -4,61 +4,46 @@ class Graph
   attr_reader :columns_left_of_mirror, :rows_above_mirror
 
   def initialize(rows)
-    # @points = get_points(rows.filter { |c| c != "" })
     @as_rows = rows.filter { |c| c != "" }.map(&:chars)
     @as_columns = @as_rows.transpose
     @rows_above_mirror = calc_mirror(@as_rows)
     @columns_left_of_mirror = calc_mirror(@as_columns)
   end
 
-  # def get_points(rows)
-  #   points = {}
-
-  #   rows.each_with_index do |r, y|
-  #     r.chars.each_with_index do |c, x|
-  #       points["#{x}:#{y}"] = c
-  #     end
-  #   end
-
-  #   points
-  # end
-
   def calc_mirror(graph)
-    # if graph.size == 15
-    #   puts "graph"
-    #   puts graph.inspect
-    # end
     split = graph.slice_when do |a, b|
       a == b
     end.to_a
 
-    # if graph.size == 15
-    #   puts "split"
-    #   puts split.inspect
-    # end
-
-    if split.size == 2 
+    length = split.size
+    if length == 1
+      return 0
+    end    
+    
+    while split.size > 2
       a = split[0].to_set
       b = split[1].to_set
+
       if a < b || b < a
-        # one is a subset
-        return a.size
+        split[1] = split[1].concat(split[2])
+        split.delete_at(2)
+      else
+        split[0] = split[0].concat(split[1])
+        split.delete_at(1)
       end
     end
 
-    if split.size > 3
-      # a = split[0].to_set
-      # b = split[1].to_set
-      # if a < b || b < a
-      #   # one is a subset
-      #   return a.size
-      # end
-      puts "split size"
-      puts split.size
-      puts split.inspect
+    if split.size != 2 
+      return 0
     end
-
-    0
+      
+    a = split[0].to_set
+    b = split[1].to_set
+    if a < b || b < a
+      split[0].size
+    else
+      0
+    end
   end
 end
 
@@ -73,9 +58,6 @@ def part1
   columns_left_of_mirror = 0
   rows_above_mirror = 0
   graphs.each_with_index do |g, i|
-    # puts "Graph #{i+1}"
-    # puts "columns_left_of_mirror #{g.columns_left_of_mirror}"
-    # puts "rows_above_mirror #{g.rows_above_mirror}"
     columns_left_of_mirror += g.columns_left_of_mirror
     rows_above_mirror += g.rows_above_mirror
   end
@@ -84,3 +66,5 @@ def part1
   puts columns_left_of_mirror + (100 * rows_above_mirror)
 end
 part1
+
+# I'm not doing part 2
